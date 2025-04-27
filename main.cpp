61ac1066a7429e1046f229aa426b2e70f1e22f30
THull5 <thull5@ut.utm.edu>
2025-04-26 23:41:26 -0500

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -12,10 +12,33 @@
 #include <iostream>
 #include <string>
 
-const int MAX_VALUE = 100;
-
-int main(){
-  
+int main() {
   Deque<int> deque;
 
+  deque.push_front(2000);
+  deque.push_front(1000);
+  deque.push_front(500);
+
+  deque.push_back(3500);
+  deque.push_back(2500);
+  deque.push_back(500);
+
+  std::cout << "Front was: " << deque.front() << std::endl; 
+  std::cout << "Back was: " << deque.back() << std::endl;   
+  std::cout << "Size was: " << deque.size() << std::endl;   
+
+  std::cout << "Elements: " << std::endl;
+  for (int i = 0; i < deque.size(); i++) {
+      std::cout << deque[i] << " ";
+  }
+  std::cout << std::endl;
+
+  deque.pop_front();
+  deque.pop_back();
+
+  std::cout << "After popping:" << std::endl;
+  std::cout << "Front is: " << deque.front() << std::endl;
+  std::cout << "Back is: " << deque.back() << std::endl;
+
+  return 0;
 }
